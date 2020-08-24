3328c70030883da04ec1f570e1878748a65e3d86
lucascalsilva <lucasc.alm.silva@gmail.com>
2020-08-24 10:48:50 +0200

    Added solution for exercise 10

---
+++ src/main/java/com/camunda/training/paymentservice/worker/PaymentFinishWorker.java
@@ -0,0 +1,78 @@
+package com.camunda.training.paymentservice.worker;
+
+import com.camunda.training.paymentservice.dto.MessageRequestDto;
+import com.camunda.training.paymentservice.dto.VariableDto;
+import org.camunda.bpm.client.ExternalTaskClient;
+import org.camunda.bpm.client.task.ExternalTask;
+import org.camunda.bpm.client.task.ExternalTaskService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static com.camunda.training.paymentservice.dto.VariableDto.fromVariableTyped;
+
+@Component
+public class PaymentFinishWorker implements CommandLineRunner {
+
+    private final ExternalTaskClient externalTaskClient;
+    private final RestTemplate restTemplate;
+    private final Logger log = LoggerFactory.getLogger(getClass().getName());
+    private static String CAMUNDA_REST_API_URL = "http://localhost:8080/engine-rest";
+    private static String CAMUNDA_MESSAGE_API = "/message";
+
+    public PaymentFinishWorker(ExternalTaskClient externalTaskClient, RestTemplate restTemplate) {
+        this.externalTaskClient = externalTaskClient;
+        this.restTemplate = restTemplate;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        externalTaskClient.subscribe("payment-finishing")
+                .handler(this::finishPayment).open();
+
+    }
+
+    private void finishPayment(ExternalTask externalTask, ExternalTaskService externalTaskService) {
+        log.info("Completing the task {}, in topic {} for process instance id {}", externalTask.getActivityId(), externalTask.getTopicName(), externalTask.getProcessInstanceId());
+        MessageRequestDto messageRequestDto = new MessageRequestDto();
+        Map<String, VariableDto> variables = new HashMap<String, VariableDto>();
+
+        variables.put("paymentOk", fromVariableTyped(externalTask.getVariableTyped("paymentOk")));
+
+        messageRequestDto.setBusinessKey(externalTask.getVariable("orderId"));
+        messageRequestDto.setMessageName("PaymentReceivedMessage");
+        messageRequestDto.setProcessVariables(variables);
+
+        ResponseEntity<String> response = restTemplate.postForEntity(CAMUNDA_REST_API_URL + CAMUNDA_MESSAGE_API, messageRequestDto, String.class);
+
+        if(response.getStatusCode().equals(HttpStatus.NO_CONTENT) || response.getStatusCode().equals(HttpStatus.OK)){
+            externalTaskService.complete(externalTask);
+        }
+        else{
+            Integer retries = getRetries(externalTask);
+
+            externalTaskService.handleFailure(externalTask,
+                    response.getStatusCode().toString(), response.getBody(), retries, 10000);
+        }
+    }
+
+    private Integer getRetries(ExternalTask externalTask) {
+        Integer retries = externalTask.getRetries();
+
+        if(retries == null){
+            retries = 3;
+        }
+        else{
+            retries -= 1;
+        }
+        return retries;
+    }
+}
